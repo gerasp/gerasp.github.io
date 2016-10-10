1316a859ea8b59c511140696f1cc231d7920c1ae
Gerardo Medina <gera.mdlh@gmail.com>
2016-10-10 22:13:26 +0100

    Only needs translations

--- js/lang.js
+++ js/lang.js
@@ -2,53 +2,57 @@
  * Created by gerardo on 03.10.16.
  */
 
-$(function () {
-    $( document ).ready(function() {
-        $("h1,h2,h3,h4,h5,h6,p,a,strong,span").each(function () {
-            $(this).data('english', $(this).html());
-        });
+$(document).ready(function () {
+    $("h1,h2,h3,h4,h5,h6,p,a,strong,span").each(function () {
+        $(this).data('english', $(this).html());
     });
+});
 
-    $(".change-lang").click(function () {
-        var langBefore = document.documentElement.lang;
-        var langAfter;
+$(".change-lang").click(function () {
+    var langBefore = document.documentElement.lang;
+    var langAfter;
 
-        if ($(this).hasClass('change-lang-es')) {
-            langAfter = "es";
-        } else if ($(this).hasClass('change-lang-de')) {
-            langAfter = "de";
+    if ($(this).hasClass('change-lang-es')) {
+        langAfter = "es";
+    } else if ($(this).hasClass('change-lang-de')) {
+        langAfter = "de";
+    } else {
+        langAfter = "en";
+    }
+
+    console.log ("Change to "+langAfter);
+
+    if (langBefore != langAfter) {
+        if(langAfter == 'en') {
+            $("h1,h2,h3,h4,h5,h6,p,a,strong,span").each(function () {
+                $(this).html($(this).data('english'));
+            });
+        } else if (langBefore == 'en') {
+            $.getJSON('js/lang/' + langAfter + '.json', function (data) {
+                $("h1,h2,h3,h4,h5,h6,p,a,strong,span").each(function () {
+                    if ($(this).text() in data.token) {
+                        $(this).text(data.token[$(this).text()]);
+                    }
+                    if ($(this).attr("id") in data.token) {
+                        $(this).text(data.token[$(this).attr("id")]);
+                    }
+                })
+            });
         } else {
-            langAfter = "en";
-        }
-
-        console.log ("Change to "+langAfter);
-
-        if (langBefore != langAfter) {
-            if(langAfter == 'en') {
+            $("h1,h2,h3,h4,h5,h6,p,a,strong,span").each(function () {
+                $(this).html($(this).data('english'));
+            });
+            $.getJSON('js/lang/' + langAfter + '.json', function (data) {
                 $("h1,h2,h3,h4,h5,h6,p,a,strong,span").each(function () {
-                    $(this).html($(this).data('english'));
-                });
-            } else if (langBefore == 'en') {
-                $.getJSON('js/lang/' + langAfter + '.json', function (data) {
-                    $("h1,h2,h3,h4,h5,h6,p,a,strong,span").each(function () {
-                        if ($(this).text() in data.token) {
-                            $(this).text(data.token[$(this).text()]);
-                        }
-                    })
-                });
-            } else {
-                $("h1,h2,h3,h4,h5,h6,p,a,strong,span").each(function () {
-                    $(this).html($(this).data('english'));
-                });
-                $.getJSON('js/lang/' + langAfter + '.json', function (data) {
-                    $("h1,h2,h3,h4,h5,h6,p,a,strong,span").each(function () {
-                        if ($(this).text() in data.token) {
-                            $(this).text(data.token[$(this).text()]);
-                        }
-                    })
-                });
-            }
-            document.documentElement.lang = langAfter;
+                    if ($(this).text() in data.token) {
+                        $(this).text(data.token[$(this).text()]);
+                    }
+                    if ($(this).attr("id") in data.token) {
+                        $(this).text(data.token[$(this).attr("id")]);
+                    }
+                })
+            });
         }
-    });
+        document.documentElement.lang = langAfter;
+    }
 });
